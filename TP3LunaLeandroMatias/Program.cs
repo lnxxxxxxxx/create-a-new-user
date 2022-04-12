19b4fae8f85090e670dbbbe72289a06065294e37
lnxxxxxx <74507760+xl25x@users.noreply.github.com>
2022-04-12 13:54:08 -0300

    Add files via upload

---
+++ TP3LunaLeandroMatias/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3LunaLeandroMatias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string email;
+            int sueldo;
+            string sexo;
+            int peso;
+            float altura;
+
+            Console.WriteLine("Bienvenido a la creacion de usuario");
+            Console.WriteLine("Ingrese el nombre:");
+            nombre = Console.ReadLine();
+
+            
+
+            Console.WriteLine("Ingrese su sueldo:");
+            sueldo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su genero:");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su peso:");
+            peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura:");
+            altura = float.Parse(Console.ReadLine());
+
+
+            Usuario miUser = new Usuario(nombre, sueldo, sexo, peso, altura);
+            Console.WriteLine("");
+            Console.WriteLine("Nombre: " + miUser.GetNombre());
+            Console.WriteLine("Email: " + miUser.GetEmail());
+            Console.WriteLine("Sueldo: " + miUser.GetSueldo() + "ARS");
+            Console.WriteLine("Sexo: " + miUser.GetSexo());
+            Console.WriteLine("Peso: " + miUser.GetPeso() + "kg");
+            Console.WriteLine("Altura: " + miUser.GetAltura() + "m");
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ TP3LunaLeandroMatias/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3LunaLeandroMatias
+{
+    class Usuario
+    {
+        private string nombre;
+        private string email;
+        private int sueldo;
+        private string sexo;
+        private int peso;
+        private float altura;
+
+        public Usuario(string nombre, int sueldo, string sexo, int peso, float altura)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+
+        }
+
+      // public Email(string email)
+        //{
+          //  this.email = nombre;
+        //}
+
+        public string GetEmail()
+        {
+            return this.email = nombre + "@superempresa.com.ar";
+        }
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public string GetSueldo()
+        {
+            return this.sueldo.ToString();
+        }
+
+        public string GetSexo()
+        {
+            return sexo;
+        }
+
+        public string GetPeso()
+        {
+            return this.peso.ToString();
+        }
+
+        public string GetAltura()
+        {
+            return this.altura.ToString();
+        }
+
+
+
+
+
+
+
+
+    }
+}
